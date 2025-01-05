e474ec7669d27587e9aa543793ab2005628795e5
haibkhn <hainguyen2000.bkhn@gmail.com>
2025-01-05 03:19:12 +0100

    fix route error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { CreateGameComponent } from './components/create-game/create-game.component';
-import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   {
     path: '',
     component: GameComponent,
   },
-  { path: 'create', component: CreateGameComponent },
+  {
+    path: 'create',
+    component: CreateGameComponent,
+  },
   {
     path: 'play/:id',
     component: GameComponent,
-    data: {
-      RenderMode: 'client',
-    },
   },
 ];
